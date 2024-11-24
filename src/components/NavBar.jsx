395c432a815760378ae98884c47b4bb335cdf447
Josefina PM <josefina.perez.mercader@gmail.com>
2024-11-24 19:43:45 -0400

    commit 24 Nov

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,57 +1,34 @@
-import React from 'react';
-import CartWidget from './CartWidget';
-import { Menu, MenuList, MenuButton, MenuItem, Flex, Box, Spacer, Button, Image } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
-import { ChevronDownIcon } from '@chakra-ui/icons';
-import logo from '/Users/josep/Desktop/Web_Development/ReactJs/Patricia_Manfredi/img/Patricia Manfredi Logo 2.png';
-import '/Users/josep/Desktop/Web_Development/ReactJs/Patricia_Manfredi/style/style.css';
+import React from "react";
+import { Flex, Box, Image, Link } from "@chakra-ui/react";
+import logo from "/Users/josep/Desktop/Web_Development/ReactJs/Patricia_Manfredi/img/Patricia Manfredi Logo 2.png";
+import "/Users/josep/Desktop/Web_Development/ReactJs/Patricia_Manfredi/style/style.css";
 
 const NavBar = () => {
   return (
-    <Flex align="center" p="4" bg="gray.100" width="100%" boxShadow="md" className="navbar">
-      <Box>
-        <Link to="/">
-          <Image src={logo} alt="Logo Patricia Manfredi" boxSize="100px" className="navbar-logo" />
+    <Flex className="navbar-container">
+      {/* Logo */}
+      <Link href="/" className="navbar-logo">
+        <Image src={logo} alt="Logo Patricia Manfredi" className="logo" />
+      </Link>
+
+      {/* Menu */}
+      <Flex className="navbar-links">
+        <Link href="/cursos" className="navbar-link">
+          Cursos Online
         </Link>
-      </Box>
-      <Spacer />
-
-      <Flex align="center" gap="8" className="navbar-links" fontFamily="'Basics Serif Free', serif">
-        <Button as={Link} to="/cursos" variant="link" className="navbar-button">
-          Cursos-online
-        </Button>
-
-        <Menu>
-          <MenuButton as={Button} rightIcon={<ChevronDownIcon />} className="navbar-button">
-            Moldeteca
-          </MenuButton>
-          <MenuList>
-            <Link to={`/modeteca/category/${"Danza"}`}><MenuItem className="navbar-menu-item">Danza</MenuItem></Link>
-            <Link to={`/moldeteca/category/${"Ropa"}`}><MenuItem className="navbar-menu-item">Ropa</MenuItem></Link>
-            <Link to={`/moldeteca/category/`}><MenuItem className="navbar-menu-item">Todos</MenuItem></Link>
-          </MenuList>
-        </Menu>
-
-        <Button as={Link} to="/merceria" variant="link" className="navbar-button">
-          Merceria
-        </Button>
-
-        <Button as={Link} to="/quienes-somos" variant="link" className="navbar-button">
-          Quienes Somos
-        </Button>
-        <Button as={Link} to="/blog" variant="link" className="navbar-button">
+        <Link href="/moldeteca" className="navbar-link">
+          Moldeteca
+        </Link>
+        <Link href="/merceria" className="navbar-link">
+          Mercería
+        </Link>
+        <Link href="/quienes-somos" className="navbar-link">
+          Quiénes Somos
+        </Link>
+        <Link href="/blog" className="navbar-link">
           Blog
-        </Button>
-        <Button as={Link} to="/en" variant="link" className="navbar-button">
-          EN
-        </Button>
+        </Link>
       </Flex>
-
-      <Spacer />
-
-      <Box>
-        <CartWidget />
-      </Box>
     </Flex>
   );
 };
